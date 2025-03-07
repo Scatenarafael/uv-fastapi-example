b1c668881f7bbc4528463b8dcca24f6d4c81265d
Scatenarafael <rafascatena@gmail.com>
2025-03-06 23:19:44 -0300

    install sqlalchemyutils

--- db/db_setup.py
+++ db/db_setup.py
@@ -3,24 +3,35 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres@db/fastapi_db"
-ASYNC_SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres@db/fastapi_db"
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql+psycopg2://postgres:postgres@localhost:5442/fastapi_db"
+)
+
+
+ASYNC_SQLALCHEMY_DATABASE_URL = (
+    "postgresql+asyncpg://postgres:postgres@localhost:5442/fastapi_db"
+)
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={}, future=True)
 
+
 async_engine = create_async_engine(ASYNC_SQLALCHEMY_DATABASE_URL)
+
 
 SessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine, future=True
 )
+
+
 AsyncSessionLocal = sessionmaker(
     async_engine, class_=AsyncSession, expire_on_commit=False
 )
 
+
 Base = declarative_base()
 
 
-# DB Utilities
 def get_db():
     db = SessionLocal()
     try:
@@ -32,4 +43,3 @@ def get_db():
 async def async_get_db():
     async with AsyncSessionLocal() as db:
         yield db
-        await db.commit()
