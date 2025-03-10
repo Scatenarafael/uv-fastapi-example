57bede74e4e88013c28c4d558fbf084e68ce4b51
Scatenarafael <rafascatena@gmail.com>
2025-03-09 22:26:01 -0300

    next

--- db/db_setup.py
+++ db/db_setup.py
@@ -1,29 +1,15 @@
-import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-POSTGRES_SERVER = os.getenv("POSTGRES_SERVER")  # PostgreSQL server address
-POSTGRES_PORT = os.getenv("POSTGRES_PORT")  # Default PostgreSQL port
-POSTGRES_USER = os.getenv("POSTGRES_USER")  # PostgreSQL username
-POSTGRES_PASSWORD = os.getenv(
-    "POSTGRES_PASSWORD"
-)  # PostgreSQL password (default empty)
-POSTGRES_DB = os.getenv("POSTGRES_DB")  # PostgreSQL database name
-
-print("POSTGRES_SERVER >>", POSTGRES_SERVER)
-print("POSTGRES_PORT >>", POSTGRES_PORT)
-print("POSTGRES_USER >>", POSTGRES_USER)
-print("POSTGRES_PASSWORD >>", POSTGRES_PASSWORD)
-print("POSTGRES_DB >>", POSTGRES_DB)
-
-SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql+psycopg2://postgres:postgres@192.168.15.6:5432/fastapi_db"
+)
 
 
 ASYNC_SQLALCHEMY_DATABASE_URL = (
-    "postgresql+asyncpg://postgres:postgres@localhost:5442/fastapi_db"
+    "postgresql+asyncpg://postgres:postgres@192.168.15.6:5432/fastapi_db"
 )
 
 
